cb46e30dda65b3fcf8aa9fef7a5c0d474f43175e
AldyGege <youngfather86@gmail.com>
2023-10-17 14:35:30 +0700

    Tambah CORS

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require('cors')
+app.use(cors())
 
 const bodyPs = require('body-parser');
-app.use(bodyPs.urlencoded({extended: false}))
+app.use(bodyPs.urlencoded({ extended: false }))
 app.use(bodyPs.json());
 
 const mhsRouter = require('./routes/mahasiswa');
@@ -12,6 +14,6 @@ app.use('/api/mhs', mhsRouter);
 const jurusanRouter = require('./routes/jurusan');
 app.use('/api/jurusan', jurusanRouter);
 
-app.listen(port,() => {
+app.listen(port, () => {
     console.log(`Aplikasi Aldy Geming Berjalan di http:://localhost:${port}`)
 })
